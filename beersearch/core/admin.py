273b4dbead38155df88bc2c7b8810d56654bfc2b
Alexandre Odoni <alexandre.odoni@cmc.pr.gov.br>
2017-05-30 09:31:38 -0300

    cadastro de fabrica de fermentos e fermentos. closes #9 e closes #10

--- beersearch/core/admin.py
+++ beersearch/core/admin.py
@@ -3,12 +3,16 @@
 from __future__ import unicode_literals
 from django.contrib import admin
 
-from .models import Loja, TipoLoja, FabricaMalte, Malte, Lupulo
+from .models import Loja, TipoLoja, FabricaMalte, Malte, Lupulo, Fermento, FabricaFermento
 
 class TipoLojaInline(admin.TabularInline):
 	model = TipoLoja.lojas.through
 	extra = 2
 	verbose_name_plural = 'Tipos de Lojas'
+
+class FermentoInline(admin.TabularInline):
+	model = Fermento
+	extra = 2	
 
 class LojaAdmin(admin.ModelAdmin):
 	model = Loja
@@ -23,9 +27,22 @@ class MalteAdmin(admin.ModelAdmin):
 	ordering = ('fabrica__nome', 'nome', )
 	search_fields = ('nome', )
 
+class FabricaFermentoAdmin(admin.ModelAdmin):
+	model = FabricaFermento
+	inlines = [FermentoInline]
+
+class FermentoAdmin(admin.ModelAdmin):
+	model = Fermento
+	list_filter = ['fabrica__nome', ]
+	ordering = ('fabrica__nome', 'nome', )
+	search_fields = ('nome', )	
+
 
 admin.site.register(Loja, LojaAdmin)
 admin.site.register(TipoLoja)
 admin.site.register(FabricaMalte)
 admin.site.register(Malte, MalteAdmin)
 admin.site.register(Lupulo)
+admin.site.register(Fermento, FermentoAdmin)
+admin.site.register(FabricaFermento, FabricaFermentoAdmin)
+
